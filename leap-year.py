9b71449aa9ac9a00e77d7f536ad5f12339622bb6
infotechia <iaktas9094@gmail.com>
2021-11-23 21:17:39 +0300

    update

--- leap-year.py
+++ leap-year.py
@@ -1,9 +1,14 @@
-year=int(input("Enter year to be checked:"))
-if(year%4==0 and year%100!=0 or year%400==0):
-   
-    print("THIS IS JUST A LEAP YEAR!)
+# To get year (integer input) from the user
+# This is my first explanation
+year = int(input("Enter a year: "))
+
+if (year % 4) == 0:
+    if (year % 100) == 0:
+        if (year % 400) == 0:
+            print("{0} is a leap year".format(year))
+        else:
+            print("{0} is not a leap year".format(year))
+    else:
+        print("{0} is a leap year".format(year))
 else:
-    print("The year isn't a leap year!)
-          
-          
-          hello
+    print("{0} is not a leap year".format(year))
