652076962b5b1b195943894c2d63c90c5e49eab4
MuratYarali <murat.yarali@outlook.com>
2021-11-28 15:54:24 +0300

    dev branch i üzerinde leap year değişikliği yapıldı

--- leap-year.py
+++ leap-year.py
@@ -2,6 +2,8 @@ year=int(input("Enter year to be checked:"))
 if(year%4==0 and year%100!=0 or year%400==0):
    
     print("THIS IS JUST A LEAP YEAR!)
+elif year%2==0:
+    print(  Dev branchinde yazıldı")
 else:
     print("The year isn't a leap year!)
           
