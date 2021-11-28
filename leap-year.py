b4d27cf64831ba973c4ff8dafac7090e306dc291
Murat Yaralı <93635779+MuratYarali@users.noreply.github.com>
2021-11-28 19:56:36 +0300

    Update leap-year.py

--- leap-year.py
+++ leap-year.py
@@ -1,9 +1,10 @@
 year=int(input("Enter year to be checked:"))
 if(year%4==0 and year%100!=0 or year%400==0):
-   
-    print("THIS IS JUST A LEAP YEAR!)
+   print("THIS IS JUST A LEAP YEAR!)
+elif year%3==0:
+   print("Bu değişiklik Remote Dev'de yapılmıştır")
 else:
-    print("The year isn't a leap year!)
+  print("The year isn't a leap year!)
           
           
           hello
