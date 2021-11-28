9afcbb4cbad635714db61913e99e1e98b1e6d11d
Murat Yaralı <93635779+MuratYarali@users.noreply.github.com>
2021-11-28 16:37:29 +0300

    Update leap-year.py

--- leap-year.py
+++ leap-year.py
@@ -2,7 +2,7 @@ year=int(input("Enter year to be checked:"))
 if(year%4==0 and year%100!=0 or year%400==0):
    print("THIS IS JUST A LEAP YEAR!)
 elif year%3==0:
-   print("Github dev branch'te yazıldı")
+   print("Burası Github dev branch'te yazıldı")
 else:
     print("The year isn't a leap year!)
           
