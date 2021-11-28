0d30280b63e981cb3adc58fc7635360f3f188e9e
TuncayArican <tuncayarican07@gmail.com>
2021-11-28 19:45:56 +0300

    Murat branchinde değişiklik yapıldı

--- leap-year.py
+++ leap-year.py
@@ -1,7 +1,10 @@
 year=int(input("Enter year to be checked:"))
 if(year%4==0 and year%100!=0 or year%400==0):
    
-    print("THIS IS JUST A LEAP YEAR!)
+  
+  print("THIS IS JUST A LEAP YEAR!)
+elif year%2==0:
+    print (bu değişkilik locak Murat da yapıldı)
 else:
     print("The year isn't a leap year!)
           
