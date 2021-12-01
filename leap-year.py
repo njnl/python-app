3b628d02416e0eef659f354a091d186d4b86d1ed
Murat Yaralı <93635779+MuratYarali@users.noreply.github.com>
2021-12-01 13:43:27 +0300

    Update leap-year.py

--- leap-year.py
+++ leap-year.py
@@ -1,7 +1,8 @@
 year=int(input("Enter year to be checked:"))
 if(year%4==0 and year%100!=0 or year%400==0):
-   
     print("THIS IS JUST A LEAP YEAR!)
+elif year%5==0:
+    print("Bu satır Ahmet tarafından güncellendi")
 else:
     print("The year isn't a leap year!)
           
