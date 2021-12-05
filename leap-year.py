f71a1db690232f11e8b3fd2f282af2beba751edf
d-1320hasan <93793537+d-1320hasan@users.noreply.github.com>
2021-12-05 18:56:11 +0300

    Update leap-year.py

--- leap-year.py
+++ leap-year.py
@@ -1,7 +1,7 @@
 year=int(input("Enter year to be checked:"))
 if(year%4==0 and year%100!=0 or year%400==0):
-   
-    print("THIS IS JUST A LEAP YEAR!)
+elif year% == 0:
+      print("bu satır ahmet tarafından yapıldı"
 else:
     print("The year isn't a leap year!)
           
