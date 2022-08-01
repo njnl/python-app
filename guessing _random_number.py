f3e7453c705957c15955a140d727231769d8dfe2
halil <leaands25@gmail.com>
2022-08-01 16:32:40 +0200

    adding condition

--- guessing _random_number.py
+++ guessing _random_number.py
@@ -5,3 +5,10 @@ number = random.randrange(100)
 # print(number)
 
 guess = int(input('Enter a number between 0-100 : '))
+
+if guess > number:
+    print('A little lower...')
+elif guess < number:
+    print('A little higher...')
+else:
+    print('You are mind reader!!!')
